69deea3af34874b379aa2504ed34303e7121cf3b
Marcus Otterström <35617441+MarcusOtter@users.noreply.github.com>
2023-01-08 14:29:54 +0100

    Asteroid work

--- orBIT/Assets/Scripts/Rotate.cs
+++ orBIT/Assets/Scripts/Rotate.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    [SerializeField] private float randomizeMaxSpeed;
     [SerializeField] private Vector3 speed;
 
+    private void Awake()
+    {
+        if (randomizeMaxSpeed == 0) return;
+        
+        speed.x = Random.Range(-randomizeMaxSpeed, randomizeMaxSpeed);
+        speed.y = Random.Range(-randomizeMaxSpeed, randomizeMaxSpeed);
+        speed.z = Random.Range(-randomizeMaxSpeed, randomizeMaxSpeed);
+    }
+    
     private void Update()
     {
         transform.Rotate(speed * Time.deltaTime);
